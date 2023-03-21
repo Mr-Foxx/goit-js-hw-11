61a5cef1fdd2f9bd451b3b08d6d6d2f96576f851
Mr-Foxx <115030615+Mr-Foxx@users.noreply.github.com>
2023-03-21 20:44:11 +0300

    looks good

--- src/index.js
+++ src/index.js
@@ -1,41 +1,104 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import $ from 'jquery';
 
 const form = document.querySelector('.search-form');
 const inputElement = document.querySelector('input[name="searchQuery"]');
-const btn = document.querySelector('button');
+
 const container = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
 
 const URL = 'https://pixabay.com/api/';
 const KEY = '34551974-263ab9c7e5b8efeaa679c471a';
 
+let page = 1;
+
+loadMore.classList.add('is-hidden');
+
 form.addEventListener('submit', handleSearch);
+loadMore.addEventListener('click', heandleLoadMorePictures);
+
+infiniteІcroll();
 
 async function handleSearch(evt) {
   evt.preventDefault();
 
   const inputValue = inputElement.value.toLowerCase().trim();
 
-  const picturesData = await fetchPictures(inputValue);
-  renderPictures(picturesData.hits);
+  try {
+    const picturesData = await fetchPictures(inputValue);
+    clearForm();
+    renderPictures(picturesData.hits);
+
+    loadMore.classList.remove('is-hidden');
+
+    if (picturesData.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMore.classList.add('is-hidden');
+    } else {
+      Notiflix.Notify.success(
+        `Hooray! We found ${picturesData.totalHits} images.`
+      );
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(error);
+    throw error;
+  }
+}
+
+async function heandleLoadMorePictures() {
+  const inputValue = inputElement.value.toLowerCase().trim();
+
+  try {
+    const resultData = await fetchPictures(inputValue);
+
+    renderPictures(resultData.hits);
+
+    page += 1;
+
+    if (resultData.hits.length === resultData.totalHits) {
+      loadMore.classList.add('is-hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    pageScrolling();
+  } catch (error) {
+    Notiflix.Notify.failure(error);
+    throw error;
+  }
 }
 
 async function fetchPictures(inputValue) {
-  const response = await fetch(
-    `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
+  try {
+    const response = await axios.get(`${URL}`, {
+      params: {
+        key: KEY,
+        q: inputValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: page,
+      },
+    });
 
-  if (!response.ok) {
+    return response.data;
+  } catch (error) {
     Notiflix.Notify.failure(error);
+    throw error;
   }
-
-  const result = await response.json();
-  return result;
 }
 
 function renderPictures(picturesData) {
   console.log(picturesData);
-  const pictur = picturesData
+
+  const newPictures = picturesData
     .map(elem => {
       const {
         webformatURL,
@@ -47,103 +110,53 @@ function renderPictures(picturesData) {
         downloads,
       } = elem;
       return `
-    <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes <span class="span">${likes}</span> </b>
-    </p>
-    <p class="info-item">
-      <b>Views <span class="span">${views}</span> </b>
-    </p>
-    <p class="info-item">
-      <b>Coments <span class="span">${comments}</span> </b>
-    </p>
-    <p class="info-item">
-      <b>Downloads <span class="span">${downloads}</span> </b>
-    </p>
-  </div>
-</div>
+        <a href="${largeImageURL}" class="photo-card">
+          <img class='img' src="${webformatURL}" alt="${tags}" loading="lazy" data-src="${largeImageURL}" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes <span class="span">${likes}</span> </b>
+            </p>
+            <p class="info-item">
+              <b>Views <span class="span">${views}</span> </b>
+            </p>
+            <p class="info-item">
+              <b>Coments <span class="span">${comments}</span> </b>
+            </p>
+            <p class="info-item">
+              <b>Downloads <span class="span">${downloads}</span> </b>
+            </p>
+          </div>
+        </a>
       `;
     })
     .join('');
 
-  container.innerHTML = pictur;
+  container.insertAdjacentHTML('beforeend', newPictures);
+
+  const lightbox = new simpleLightbox('.gallery a');
+}
+
+function pageScrolling() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+function clearForm() {
+  container.innerHTML = '';
+}
+
+function infiniteІcroll() {
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+      heandleLoadMorePictures();
+    }
+  });
 }
 
 // ================================
-
-// const form = document.querySelector('.search-form');
-// const inputElement = document.querySelector('input[name="searchQuery"]');
-// const btn = document.querySelector('button');
-// const container = document.querySelector('.gallery');
-
-// const URL = 'https://pixabay.com/api/';
-// const KEY = '34551974-263ab9c7e5b8efeaa679c471a';
-
-// form.addEventListener('submit', heandleSearch);
-
-// function heandleSearch(evt) {
-//   evt.preventDefault();
-
-//   const inputValue = inputElement.value.toLowerCase().trim();
-
-//   fetchPictures(inputValue)
-//     .then(data => {
-//       renderPictures(data.hits);
-//     })
-//     .catch(error => console.log(error));
-// }
-
-// function fetchPictures(inputValue) {
-//   return fetch(
-//     `${URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`
-//   )
-//     .then(response => response.json())
-//     .then(data => {
-//       // тут можна обробити отримані дані
-//       console.log(data);
-//       return data;
-//     });
-// }
-
-// function renderPictures(picturesData) {
-//   console.log(picturesData);
-
-//   const pictur = picturesData
-//     .map(elem => {
-//       const {
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       } = elem;
-//       return `
-//     <div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${comments}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>${downloads}</b>
-//     </p>
-//   </div>
-// </div>
-//       `;
-//     })
-//     .join('');
-
-//   container.innerHTML = pictur;
-// }
-
-// ===================
